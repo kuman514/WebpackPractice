68db849bf16f708d0b242b3d6e168b28c77c6a9f
kumankoishi.py <7iggif@naver.com>
2021-09-30 05:17:23 +0900

    Added real clock component

--- js/clock.js
+++ js/clock.js
@@ -1,8 +1,14 @@
-function paintTime() {
-  document.querySelector('main').textContent = new Date();
-}
+import Component from './component';
 
-export default function clock() {
-  paintTime();
-  setInterval(paintTime, 1000);
-};
+export default class Clock extends Component {
+  constructor(props, state) {
+    super(props, state);
+
+    this.rootElement = document.createElement('div');
+    this.rootElement.className  = 'clock';
+    this.render = () => {
+      this.rootElement.textContent = this.state.date;
+      return this.rootElement;
+    };
+  }
+}

f8bc9729c685de1e82a512025ffce316090d4a3d
kumankoishi.py <7iggif@naver.com>
2021-10-02 00:23:27 +0900

    Added functions of buttons

--- js/buttons.js
+++ js/buttons.js
@@ -6,7 +6,28 @@ export default class Buttons extends Component {
 
     this.rootElement = document.createElement('div');
     this.rootElement.className = 'buttons';
+    this.rootElement.addEventListener('click', (event) => {
+      const parsedId = parseInt(event.target.id);
+      if (parsedId === null || parsedId === undefined) {
+        return;
+      }
+      this.props.onClick(this.props.modes[parsedId]);
+    });
+
     this.render = () => {
+      const buttons = Array.from({
+        length: 4
+      }, (_, index) => {
+        const button = document.createElement('button');
+        button.id = index;
+        button.textContent = this.props.modes[index];
+        return button;
+      });
+      
+      buttons.forEach((item) => {
+        this.rootElement.appendChild(item);
+      });
+
       return this.rootElement;
     };
   }

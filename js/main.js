b617c73d01cd1bcb3a9ae8fdef499685f54f6c59
kumankoishi.py <7iggif@naver.com>
2021-10-01 11:28:32 +0900

    Removed code of content

--- js/main.js
+++ js/main.js
@@ -1,50 +1,21 @@
 import Component from './component';
-import Clock from './clock';
+import Content from './content';
+import Buttons from './buttons';
 
 class Main extends Component {
   constructor(props, state) {
     super(props, state);
 
-    this.intervalId = null;
-    this.clearInterval = () => {
-      if (this.intervalId) {
-        clearInterval(this.intervalId);
-        this.intervalId = null;
-      }
-    }
-
-    this.setClockInterval = (clock) => {
-      this.intervalId = setInterval(() => {
-        clock.setState({
-          date: new Date()
-        });
-      }, 1000);
-    }
-    this.createClock = () => {
-      const clock = new Clock({}, {
-        date: new Date()
-      });
-      this.setClockInterval(clock);
-      return clock;
-    }
-
     this.rootElement = document.querySelector('main');
     this.rootElement.className  = 'main';
     this.render = () => {
-      this.rootElement.innerText = '';
-      this.clearInterval();
-
-      switch (this.state.mode) {
-        case 'clock':
-          this.rootElement.appendChild(this.createClock().render());
-          break;
-      }
+      this.rootElement.appendChild(new Content({}, {
+        mode: 'clock'
+      }).render());
 
       return this.rootElement;
     };
   }
 }
 
-(new Main({}, {
-  mode: 'clock'
-})).render();
+(new Main({}, {})).render();
